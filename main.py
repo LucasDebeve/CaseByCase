feedb40b4a0c1c56b736cfc23adbb35a275f19a0
Lucas Debeve <lucas.debeve2003@gmail.com>
2024-11-28 20:56:17 +0100

    ci: pre-commit config and CI

--- main.py
+++ main.py
@@ -4,29 +4,29 @@ from pygame.locals import QUIT
 
 
 class Game:
-    def __init__(self, width: int, height: int) -> None:
-        self.width = width
-        self.height = height
-        self.fps = 30
+	def __init__(self, width: int, height: int) -> None:
+		self.width = width
+		self.height = height
+		self.fps = 30
 
-    def run(self) -> None:
-        pygame.init()
-        screen = pygame.display.set_mode((self.width, self.height))
-        clock = pygame.time.Clock()
+	def run(self) -> None:
+		pygame.init()
+		screen = pygame.display.set_mode((self.width, self.height))
+		clock = pygame.time.Clock()
 
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    running = False
+		running = True
+		while running:
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					running = False
 
-            screen.fill((0, 0, 0))
-            pygame.display.flip()
-            clock.tick(self.fps)
+			screen.fill((0, 0, 0))
+			pygame.display.flip()
+			clock.tick(self.fps)
 
-        pygame.quit()
+		pygame.quit()
 
 
 if __name__ == "__main__":
-    game = Game(800, 600)
-    game.run()
+	game = Game(800, 600)
+	game.run()
